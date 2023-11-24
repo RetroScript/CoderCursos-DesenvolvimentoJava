4491f96109d6a25605e880641d1f3a86e9f72cc7
RetroScript <andrefrancelino06@gmail.com>
2023-11-24 20:44:21 -0300

    aula 20: comentários
    código correspondente a aula de comentários da seção "fundamentos" do curso

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -3,13 +3,33 @@ package fundamentos;
 /**
  * 
  * @author André Francelino <andrefrancelino06@gmail.com>
- *
+ * @since JDK1.0
  */
 
 public class PrimeiroPrograma {
 
-	public static void main(String[] args) {
-		System.out.println("Primeiro programa!!!");
-	}
+	/**
+	 * 
+	 * @param args
+	 */
 	
-}
+	public static void main(String[] args) {
+		
+		//esse comentário é de linha única
+		//da barra para frente tudo é comentado
+		//essa já é outra linha
+		
+		System.out.println("Primeiro programa!!!");
+		
+		/*
+		 * agora são várias linhas
+		 * linha 1
+		 * linha 2
+		 * linha 3...
+		 */
+		System.out.println("Primeiro programa!!!");
+		System.out.println("Primeiro programa!!!");
+		
+	}//posso comentar aqui também
+	
+}//e aqui também

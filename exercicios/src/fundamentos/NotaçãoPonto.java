5026ee29c6220e7d03938c1423bfc60be94177d6
RetroScript <andrefrancelino06@gmail.com>
2024-07-03 22:09:58 -0300

    aula 27: notação ponto

---
+++ exercicios/src/fundamentos/NotaçãoPonto.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class NotaçãoPonto {
+
+	public static void main(String[] args) {
+		
+		String s = "ola" + "ola";
+		s.contains("oi");
+		
+		String s1 = "OI".toLowerCase();
+		
+		String s2 = "Ola".toLowerCase().concat("!!");
+		
+		System.out.println("gg".toUpperCase());
+		
+	}
+	
+}
